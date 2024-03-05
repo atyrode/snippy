ba43f5c7e817d020eb379a9f4929f8bc0807452a
atyrode <alex.tyrode@outlook.fr>
2024-03-05 01:41:37 +0100

    Implementation of an int <-> str codec (#7)
    * implement urlcharset class + test suite #6
    
    * enforce intent for URLCharset and clearer logic #6
    
    * separation of concerns, charset class now validates input itself #6
    
    * obfuscation logic moved into own branch #6
    
    * implement [int <-> str] codec #6

---
+++ src/codec.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from charset import URLCharset
+    
+# Codec class isn't the best definition of a 'dataclass' as it contains logic
+# but it benefits from immutability without having to manually define it.
+@dataclass(frozen=True) 
+class Codec():
+    """Translates an integer to a URL-safe string and vice-versa based on
+    the provided URLCharset and its base.
+    """
+    
+    charset: URLCharset
+    
+    def __post__init__(self):        
+        if not isinstance(self.charset, URLCharset):
+            raise TypeError(f"Expected URLCharset, got {type(self.charset).__name__}")
+    
+    @property
+    def _base(self) -> int:
+        return len(self.charset)
+    
+    def encode(self, id: int) -> str:
+        encoded: str    = str()
+        remainder: int  = int()
+        
+        if id == 0:
+            return self.charset[0]
+        
+        while id > 0:
+            id, remainder = divmod(id, self._base)
+            encoded = self.charset[remainder] + encoded
+        
+        return encoded
+
+    def decode(self, encoded: str) -> int:
+        decoded: int = int()
+        
+        # Ensure the encoded string characters are valid for the charset
+        self.charset.validate(encoded)
+        
+        for char in encoded:
+            decoded = decoded * self._base + self.charset.index(char)
+
+        return decoded
